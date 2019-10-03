f3d69c40b5cf5594140917967a6692d817819065
guga <guga.burigo29@outlook.com>
2019-10-03 00:11:26 -0300

    Cria model de produto

---
+++ src/br/com/jpa/model/bean/Produto.java
@@ -0,0 +1,57 @@
+package br.com.jpa.model.bean;
+
+import javax.persistence.*;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+    private Integer quantidade;
+    private Double valor;
+
+    @ManyToOne
+    private Categoria categoria;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
